0fb038702ec8a7eadeaa50a501d5b8d1e7e43c24
SandyCOG <89305059+SandyCOG@users.noreply.github.com>
2024-01-14 04:37:37 +0100

    Update app.py

--- Home_Page/app.py
+++ Home_Page/app.py
@@ -152,7 +152,7 @@ elif state.page == "Contributors":
 
 # Charts Page
 elif state.page == "Charts":
-    df = pd.read_csv('Home_Page\preprocessed_data.csv')
+    df = pd.read_csv('Home_Page\food_price_dataset.csv')
     st.title("Food Prediction Model")
 
     # Page content 
