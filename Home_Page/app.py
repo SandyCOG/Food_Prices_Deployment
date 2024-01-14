eb497eed7c50b02e4ec071c9ddc3fcc7e7420b56
SandyCOG <89305059+SandyCOG@users.noreply.github.com>
2024-01-14 18:47:34 +0100

    Update app.py

--- Home_Page/app.py
+++ Home_Page/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 #import seaborn as sns
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 # Set Page Configuration for a Wider Layout
 st.set_page_config(layout="wide")
