af1f8517767147acaaf4887ab4c2ff7bf6834a5b
SandyCOG <89305059+SandyCOG@users.noreply.github.com>
2024-01-16 17:12:44 +0100

    Update app.py

--- Home_Page/app.py
+++ Home_Page/app.py
@@ -219,15 +219,15 @@ elif state.page == "Charts":
 
     #Plot of Crops accross each states
     st.subheader("Crops across each State", divider='rainbow')
-    #selected_crop = st.selectbox('Select Crop Type:', df['crops'].unique())
+    food_crop = st.selectbox('Select Crop Type:', df['crops'].unique())
 
     # Filter DataFrame based on selected state
-    #filtered_df = df[df['crop'] == selected_crop]
+    filtered_df = df[df['crop'] == food_crop]
 
     #st.title(f'Crop Concentration in {selected_state}')
 
     # Scatter plot
-    st.map(df['crops'].unique(), latitude='Latitude', longitude='Longitude')
+    st.map(filtered_df, latitude='Latitude', longitude='Longitude')
 
 # Feedback Page
 elif state.page == "Feedback":
