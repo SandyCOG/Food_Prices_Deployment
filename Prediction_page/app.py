74b07ef6270bd2fe3830f92734864b55f6cc96f7
SandyCOG <89305059+SandyCOG@users.noreply.github.com>
2024-01-31 16:54:11 +0100

    Update app.py

--- Prediction_page/app.py
+++ Prediction_page/app.py
@@ -25,12 +25,9 @@ model = pickle.load(open(filename, 'rb'))
 with st.sidebar:
     st.subheader('Food Crops to Predict Price')
 
-#make_model = st.sidebar.selectbox("Model Selection", ("Audi A3", "Audi A1", "Opel Insignia", "Opel Astra", "Opel Corsa", "Renault Clio", "Renault Espace", "Renault Duster"))
-#hp_kW = st.sidebar.number_input("Horse Power:",min_value=40, max_value=294, value=120, step=5)
-#age = st.sidebar.number_input("Age:",min_value=0, max_value=3, value=0, step=1)
-#km = st.sidebar.number_input("km:",min_value=0, max_value=317000, value=10000, step=5000)
-#Gears = st.sidebar.number_input("Gears:",min_value=5, max_value=8, value=5, step=1)
-#Gearing_Type = st.sidebar.radio("Gearing Type", ("Manual", "Automatic", "Semi-automatic"))
+    Date = st.text_input("Enter a date:", "default_value")
+    Crops = st.select_box("Choose crop:", df['crops'].unique())
+    State = st.select_box("Choose state:", df['states'].unique())
 
 
 #my_dict = {"make_model":make_model, "hp_kW":hp_kW, "age":age, "km":km, "Gears":Gears, "Gearing_Type":Gearing_Type}
