08771cadc744229bcc3c0879891872ef6f9dbf48
SandyCOG <89305059+SandyCOG@users.noreply.github.com>
2024-02-22 11:40:51 +0100

    Update test.py

--- Home_Page/test.py
+++ Home_Page/test.py
@@ -1,3 +1,4 @@
+df = pd.read_csv('Home_Page/food_price_dataset.csv')
 #Line plot of Annual Rainfall across each states
     st.subheader("Inflation over the Years", divider='rainbow')
-    st.line_chart(df.pivot_table(index='states', columns='Year', values='Inflation').fillna(0), height=400)
+    st.line_chart(df.pivot_table(index='states', columns='Year', values='inflation_rate(food_item monthly avg.)').fillna(0), height=400)
